0a6708a0b70f72f8e8e3c48d8ef8903f8566556b
Adri-Charbonneau <105881448+Adri-Charbonneau@users.noreply.github.com>
2022-09-02 22:30:20 +0200

    Test

--- SCRIPTS/HBW.ps1
+++ SCRIPTS/HBW.ps1
@@ -7,15 +7,17 @@ $promo = $value[1] -replace ","
 $very_original = $value[2] -replace ","
 
 $date = Get-Date -Format "yyyy-mm-dd"
+echo $date
 
 $line = "{0},{1},{2},{3}" -f  $date,$original,$promo,$very_original
+echo $line
 $line | Add-Content "./DATA/PRIX_HBW.csv"
 
 Remove-Item HBW.html
 
 # git and create tag
-git config --local user.email "a-d-r-i@outlook.fr"
-git config --local user.name "A-d-r-i"
-git add .
-git commit -m "[Bot] Update HBW price" --allow-empty
-git push -f
+# git config --local user.email "a-d-r-i@outlook.fr"
+# git config --local user.name "A-d-r-i"
+# git add .
+# git commit -m "[Bot] Update HBW price" --allow-empty
+# git push -f
