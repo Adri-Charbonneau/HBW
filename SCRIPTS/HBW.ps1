cb38bfa7485f2589fe8ee1e43ef159fec6f7324e
Adrien CHARBONNEAU <105881448+Adri-Charbonneau@users.noreply.github.com>
2024-08-08 14:18:55 +0200

    Correction

--- SCRIPTS/HBW.ps1
+++ SCRIPTS/HBW.ps1
@@ -4,24 +4,9 @@ Invoke-WebRequest -Uri "https://www.lynxeds.com/product/handbook-of-the-birds-of
 $Source = Get-Content -path HBW.html -raw
 $value = ($Source | select-string -pattern '([0-9]+,[0-9]*\.[0-9]+)' -AllMatches).Matches.Value
 
-if ( $value.count -eq 1 )
-{
-  $original = "3537.00"
-  $promo = $value -replace ","
-  $very_original = "3537.00"
-}
-elseif ( $value.count -eq 2 )
-{
-  $original = $value[0] -replace ","
-  $promo = $value[1] -replace ","
-  $very_original = "3537.00"
-}
-else
-{
-  $original = $value[0] -replace ","
-  $promo = $value[1] -replace ","
-  $very_original = $value[2] -replace ","
-}
+$original = $value[0] -replace ","
+$promo = $value[1] -replace ","
+$very_original = "3537.00"
 
 $date = Get-Date -Format "yyyy-MM-dd"
 echo $date
